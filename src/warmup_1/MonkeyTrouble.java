b9b259892f3a945aef3b57e54149b48d429b476b
Regall <michael.regall@gmail.com>
2022-03-24 12:45:28 -0500

    First commit

---
+++ src/warmup_1/MonkeyTrouble.java
@@ -0,0 +1,24 @@
+package warmup_1;
+
+/*
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+ */
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (!aSmile || bSmile) && (aSmile || !bSmile);
+    }
+    /*
+    if((aSmile &&  !bSmile)||(!aSmile && bSmile)){
+            return false;
+        }
+        return true;
+    }
+     */
+
+}
