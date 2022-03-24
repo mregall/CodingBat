c14a98354bb3670a5f1164176846f27827ee2e54
Regall <michael.regall@gmail.com>
2022-03-24 13:03:58 -0500

    Util class started

---
+++ src/warmup_1/Warmup_Util.java
@@ -0,0 +1,32 @@
+package warmup_1;
+
+public class Warmup_Util {
+
+
+    public int diff21(int n) {
+        if(n <= 21){
+            return 21-n;
+        }
+        return (n-21)*2;
+    }
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking && hour < 7) || (talking && hour > 20);
+    }
+
+    public boolean makes10(int a, int b) {
+        if(a+b==10){
+            return true;
+        }else return a == 10 || b == 10;
+    }
+    public boolean nearHundred(int n) {
+        if (n>=90 && n<=110){
+            return true;
+        }else return n >= 190 && n <= 210;
+    }
+
+
+
+}
+
+
