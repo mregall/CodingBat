55f1921b5bda84951f68047696208c65ef8cf855
Regall <michael.regall@gmail.com>
2022-04-01 12:36:21 -0500

    DoubleX

--- src/warmup_2/ArrayCount9.java
+++ src/warmup_2/ArrayCount9.java
@@ -7,8 +7,8 @@ public class ArrayCount9 {
 
     public static int arrayCount9(int[] nums) {
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 9){
+        for (int num : nums) {
+            if (num == 9) {
                 count++;
             }
         }
