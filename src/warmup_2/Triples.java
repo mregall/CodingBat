6a627876b3909ee6f16493ff7c20669812433244
mregall <michael.regall@gmail.com>
2022-04-12 13:51:15 -0500

    Array1 updates

---
+++ src/warmup_2/Triples.java
@@ -0,0 +1,11 @@
+package warmup_2;
+
+public class Triples {
+    public boolean noTriples(int[] nums) {
+        for (int i = 0; i < nums.length-2; i++) {
+            int first = nums[i];
+            if (nums[i+1]==first && nums[i+2]==first) return false;
+        }
+        return true;
+    }
+}
