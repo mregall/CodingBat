09e97605b493d31126d102882f7132e01cba4b3b
mregall <michael.regall@gmail.com>
2022-04-18 08:10:21 -0500

    April 17

--- src/dates/April17.java
+++ src/dates/April17.java
@@ -1,6 +1,8 @@
 package dates;
 
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class April17 {
 
@@ -79,7 +81,96 @@ public class April17 {
         return "";
     }
 
+    //nine
+    public int[] midThree(int[] nums) {
+        int n = nums.length/2;
+        int[] arr = new int[3];
+        arr[0] = nums[n-1];
+        arr[1] = nums[n];
+        arr[2] = nums[n+1];
+        return arr;
+    }
 
+    //ten
+    public int maxTriple(int[] nums) {
+        int max = 0;
+        int n = nums.length/2;
+        int f = nums[0];
+        int m = nums[n];
+        int e = nums[nums.length-1];
+        if (f > m) {
+            max = f;
+        }else{
+            max = m;
+        }
+        if (e > max){
+            max = e;
+        }
+        return max;
+        //this feels awful, but it worked.
+    }
+
+    //ten v2 - we hadn't learned the Math functions so that makes sense.
+    public int maxTriples(int[] nums) {
+        return Math.max(nums[0], Math.max(nums[(nums.length - 1) / 2], nums[nums.length - 1]));
+    }
+
+    //eleven
+    public int[] frontPiece(int[] nums) {
+        if (nums.length>=2){
+            int[] n = {nums[0], nums[1]};
+            return n;
+        }
+        return nums;
+    }
+
+    //twelve
+    public boolean unlucky1(int[] nums) {
+        if (nums.length<2) return false;
+        if ((nums[0] == 1) && (nums[1] == 3)) return true;
+        if ((nums[1] == 1) && (nums[2] == 3)) return true;
+        return ((nums[nums.length-2]==1) && (nums[nums.length-1]==3));
+    }
+
+    //thirteen
+    public int[] make2(int[] a, int[] b) {
+        int[] two = new int[2];
+        if (a.length>=2){
+            two[0]=a[0];
+            two[1]=a[1];
+            return two;
+        }
+        if (a.length==1){
+            two[0]=a[0];
+            two[1]=b[0];
+            return two;
+        }else{
+            two[0]=b[0];
+            two[1]=b[1];
+            return two;
+        }
+    }
+
+    //fourteen
+    public int[] front11(int[] a, int[] b) {
+        //set it to zero length, then change the length? How does this work?
+        //is it because it hasn't been used yet?
+        int[] c =new int[0];
+
+        if(a.length>0 && b.length>0){
+            c = new int[2];
+            c[0]=a[0];
+            c[1]=b[0];
+        }else if(a.length == 0 && b.length>1){
+            c = new int[1];
+            c[0]=b[0];
+        }else if(b.length == 0 && a.length>1){
+            c = new int[1];
+            c[0]=a[0];
+        }
+        return c;
+
+    }
 }
 
 
