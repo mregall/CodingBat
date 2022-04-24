f72d9e7323554f908a1c8c74ac37241bce9ad5b7
mregall <michael.regall@gmail.com>
2022-04-24 11:02:43 -0500

    April 20

--- src/dates/April22.java
+++
@@ -1,7 +0,0 @@
-package dates;
-
-public class April22 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/dates/April24.java
@@ -0,0 +1,39 @@
+package dates;
+
+import java.util.Arrays;
+
+public class April24 {
+
+    public int countEvens(int[] nums) {
+        int n = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    public int bigDiff(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-1] - nums[0];
+    }
+
+    public int centeredAverage(int[] nums) {
+        int denominator = nums.length-2;
+        Arrays.sort(nums);
+        int min = nums[0];
+        int max = nums[nums.length-1];
+
+        int numerator = -min - max;
+        for (int num : nums) {
+            numerator += num;
+        }
+        return numerator/denominator;
+    }
+
+    public int sum13(int[] nums) {
+        if (nums.length == 0) return 0;
+        return -1;
+    }
+}
